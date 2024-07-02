b483076fab713d145ebab7e9f9afcfbbcf5d9f14
tarun <993tarun@gmail.com>
2024-07-02 16:01:55 +0530

    adding files

--- index.js
+++ index.js
@@ -24,5 +24,6 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use('/', require('./routes'));
 
-server.listen(process.env.PORT || 3000);
-// console.log("working fine");
+server.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
